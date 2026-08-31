7cf9baf23cf4957899dc42104a5473ceb0660efb
MAC <jws@MACui-MacBookPro.local>
2026-08-31 11:30:22 +0900

    test: include Windows compatibility migration

--- packages/email-dms/test/outlook-desktop-installation-migration.test.js
+++ packages/email-dms/test/outlook-desktop-installation-migration.test.js
@@ -52,6 +52,7 @@ test("desktop installation migration defines tokenless tenant-RLS lifecycle auth
       "006_outlook_desktop_release_trust",
       "007_outlook_desktop_assignment",
       "008_outlook_desktop_trusted_current_read",
+      "009_outlook_desktop_legacy_windows_compatibility",
     ],
   );
   const migration = migrations.find(
